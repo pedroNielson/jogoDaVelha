bcbd567cc396213a2ac5bff4c2dad064b3f86713
pedroNielson <pedronielson@gmail.com>
2021-05-27 23:44:03 -0300

    Adicionadas funcoes para desenhar a linha do vencedor, adicionada a funcao de restart do game

--- jogoDaVelha.py
+++ jogoDaVelha.py
@@ -12,6 +12,7 @@ CROSS_SPACE = 55
 
 BLACK = (0,0,0)
 WHITE = (255,255,255)
+GREEN = (0,128,0)
 
 #Gera a tela
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
@@ -43,26 +44,45 @@ def win(player):
     #Qualquer vitoria horizontal
     for row in range(3):
         if board[row][0] == player and board[row][1] == player and board[row][2] == player:
+            draw_win_horizontal(row,player)
             return True
-        else:
-            return False
+
 
     #Qualquer vitoria vertical
     for col in range(3):
         if board[0][col] == player and board[1][col] == player and board[2][col] == player:
+            draw_win_vertical(col, player)
             return True
-        else:
-            return False
     
     #Checa vitoria nas diagonais
     if board[0][0] == player and board[1][1] == player and board[2][2] == player:
+        draw_vertical_win_des(player)
         return True
     
     if board[2][0] == player and board[1][1] == player and board[0][2] == player:
+        draw_vertical_win_sub(player)
         return True
    
     
     return False #So ira retornar false se nao houver nenhum caso de vitoria
+
+#Metodos para desenhar a linha de vitoria
+def draw_win_vertical(col, player):
+    posX = col*T_HEIGHT + T_HEIGHT/2
+
+    pygame.draw.line(screen, GREEN, (posX, 15), (posX, HEIGHT - 15), 10)
+
+def draw_win_horizontal(row, player):
+    posY = row*T_HEIGHT + T_HEIGHT/2
+
+    pygame.draw.line(screen, GREEN, (15, posY), (HEIGHT - 15,posY), 10)
+
+def draw_vertical_win_sub(player):
+    pygame.draw.line(screen, GREEN, (15, HEIGHT-15), (HEIGHT - 15,15), 10)
+
+def draw_vertical_win_des(player):
+    pygame.draw.line(screen, GREEN, (15, 15), (HEIGHT - 15,WIDTH-15), 10)
+
 
 #Criando uma matriz de zeros para representar o board vazio
 board = np.zeros((3,3))
@@ -92,8 +112,19 @@ def mark_empty_space(row,col,player):
     else:
         return False
 
-player = 1 
+#Método para reiniciar o jogo
+def restart():
+    player = 1
+    for row in range(3):
+        for col in range(3):
+            board[row][col] = 0
+    screen.fill(BLACK)
+    drawBoard()
 
+
+
+player = 1 
+final_game = False
 
 
 
@@ -103,7 +134,7 @@ while True:
         if event.type == pygame.QUIT:
             sys.exit()
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN and not final_game:
             mouseX = event.pos[0]
             mouseY = event.pos[1]
 
@@ -112,21 +143,22 @@ while True:
 
             if check_empty_space(clicked_row, clicked_col):
                 if player == 1:
-                    mark_empty_space(clicked_row,clicked_col,player)
+                    mark_empty_space(clicked_row,clicked_col,1)
+                    if win(player):
+                        final_game = True
                     player = 2
                 elif player == 2: 
-                    mark_empty_space(clicked_row,clicked_col,player)
+                    mark_empty_space(clicked_row,clicked_col,2)
+                    if win(player):
+                        final_game = True
                     player = 1
                 
                 
                 drawFigs()
-
-                if win(player):
-                    print("ganhou")
-                else:
-                    print("nada")
-
-                print(board)   
+                print(board)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r:
+                restart()   
 
 
     pygame.display.update()
